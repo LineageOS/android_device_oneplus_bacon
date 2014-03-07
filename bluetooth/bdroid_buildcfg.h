6849ce150cfddad1529d5b0eea56023ce73593cd
Steve Kondik <shade@chemlab.org>
2014-03-07 15:34:32 -0800

    bacon: Enable QCOM Bluetooth flag
    Change-Id: I1403095d97f4d735f0aa05b63ef455a6a63e1da3

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,6 +17,8 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
+#define BLUETOOTH_QCOM_SW TRUE
+
 #define BTM_DEF_LOCAL_NAME   "One+ One"
 // Enables Interleave scan
 #define BTA_HOST_INTERLEAVE_SEARCH  TRUE
