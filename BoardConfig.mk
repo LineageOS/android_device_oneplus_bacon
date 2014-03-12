446d55284f5b39417efdf77be2c843fc14c0ff6b
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-03-12 23:34:51 +0000

    nfc: Build the NFC HAL
    Change-Id: Ice507be47d678807c10a7ecb26079ca2678c2fb0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,5 +52,8 @@ TARGET_WCNSS_MAC_PREFIX := e8bba8
 # Workaround for factory issue
 BOARD_VOLD_CRYPTFS_MIGRATE := true
 
+BOARD_NFC_CHIPSET := pn547
+BOARD_NFC_HAL_SUFFIX := bacon
+
 # inherit from the proprietary version
 -include vendor/oneplus/bacon/BoardConfigVendor.mk
