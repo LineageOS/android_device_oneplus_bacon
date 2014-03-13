2d88faa8e1ac34dcf224a3d6213076ee6403f4f1
Steve Kondik <shade@chemlab.org>
2014-03-12 21:49:58 -0700

    bacon: Update build strings to something sane
    Change-Id: Ib3a016f2faf00a31a0bc30a6f6ec93c915a84be8

--- cm.mk
+++ cm.mk
@@ -1,4 +1,22 @@
-$(call inherit-product, device/oneplus/bacon/full_bacon.mk)
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from bacon device
+$(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Inherit some common CM stuff
 $(call inherit-product, vendor/cm/config/gsm.mk)
@@ -11,5 +29,11 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_bacon
 PRODUCT_DEVICE := bacon
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := One
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=4.3/JLS36C/1390381364:user/release-keys PRIVATE_BUILD_DESC="msm8974-user 4.3 JLS36C eng.root.20140122.170107 release-keys"
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BRAND := oneplus
+TARGET_VENDOR_PRODUCT_NAME := one
+TARGET_VENDOR_DEVICE_NAME := bacon
