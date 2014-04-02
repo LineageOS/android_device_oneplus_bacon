cd415637b7a4195fe6dca94f2b7245692658a544
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-04-02 18:37:29 +0100

    bacon: Add splashscreen to the OTA packages
    Change-Id: I00dc488bac20f3d37090610c07565a0225081c24

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,3 +4,7 @@ LOCAL_PATH := $(call my-dir)
 # extra images
 #----------------------------------------------------------------------
 include build/core/generate_extra_images.mk
+
+# Radio images
+$(call add-radio-file,radio/filesmap)
+$(call add-radio-file,radio/logo.bin)
