7caa37c66e686c0587e0bb20cf62942f0ffa143c
Steve Kondik <shade@chemlab.org>
2014-04-01 21:27:33 -0700

    bacon: Correct the device name
     * "OnePlus" instead of "One+"
    
    Change-Id: I104ebd669e0aaa3a46ca65627a9177e44a40ca55

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,7 +19,7 @@
 
 #define BLUETOOTH_QCOM_SW TRUE
 
-#define BTM_DEF_LOCAL_NAME   "One+ One"
+#define BTM_DEF_LOCAL_NAME   "OnePlus One"
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
 #define MAX_L2CAP_CHANNELS    14
