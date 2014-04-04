e648a40644ab64117e474f40806e938c56ab083a
Ethan Chen <intervigil@gmail.com>
2014-04-04 13:18:21 -0700

    bacon: add support for AB, AC variant
    Change-Id: I3a22ad1d630e258f30e125fc7476ae3a3184444f

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 ## THIS IS A DEFAULT: YOU SHOULD OVERRIDE IT FROM THE DEVICE-SPECIFIC
 ## BoardConfig. Check the kernel's arch/arm/boot/dts/ path for possible
 ## values.
-G2_DTS_TARGET ?= msm8974pro-ab-pm8941-a0001
+G2_DTS_TARGET ?= msm8974pro-*-a0001
 
 
 ## Don't change anything under here. The variables are named G2_whatever
