5f9fc8e488f4353352193240c8afc26c801f59cb
Ethan Chen <intervigil@gmail.com>
2014-04-30 18:10:02 -0700

    bacon: Add all firmware with SHA1 hashes for versioning
    * Firmware version: A0001_12_2.02_140416
    
    Change-Id: I677b0ea52648b70cb262554a18196cbb26b0a3ef

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -7,4 +7,10 @@ include build/core/generate_extra_images.mk
 
 # Radio images
 $(call add-radio-file,radio/filesmap)
+$(call add-radio-file,radio/NON-HLOS.bin)
+$(call add-radio-file,radio/sbl1.mbn)
+$(call add-radio-file,radio/sdi.mbn)
+$(call add-radio-file,radio/emmc_appsboot.mbn)
+$(call add-radio-file,radio/rpm.mbn)
+$(call add-radio-file,radio/tz.mbn)
 $(call add-radio-file,radio/logo.bin)
