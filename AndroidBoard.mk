1abc2220ad7bae62f5bd80f9ba85c2732eebfa18
Steve Kondik <shade@chemlab.org>
2014-06-10 13:44:01 -0700

    bacon: Removing firmware binaries
     * Moved elsewhere.
    
    Change-Id: I2adc31aa2e693d91bec1051da8f238de445b07f8

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,13 +4,3 @@ LOCAL_PATH := $(call my-dir)
 # extra images
 #----------------------------------------------------------------------
 include build/core/generate_extra_images.mk
-
-# Radio images
-$(call add-radio-file,radio/filesmap)
-$(call add-radio-file,radio/NON-HLOS.bin)
-$(call add-radio-file,radio/sbl1.mbn)
-$(call add-radio-file,radio/sdi.mbn)
-$(call add-radio-file,radio/emmc_appsboot.mbn)
-$(call add-radio-file,radio/rpm.mbn)
-$(call add-radio-file,radio/tz.mbn)
-$(call add-radio-file,radio/logo.bin)
