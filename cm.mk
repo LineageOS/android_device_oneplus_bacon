b73529f6ed24974a7e8fe3748cd01dc09d55fa9d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-06-13 03:02:01 +0100

    bacon: Use latest Google-approved identifiers for community builds
    ... but keep the generated ones when rolling new signed builds
    
    Change-Id: Ie96b4fd11c539ad0daf3803842d748a87db76be5

--- cm.mk
+++ cm.mk
@@ -42,3 +42,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
 # Bacon gets a special boot animation because it's special.
 PRODUCT_BOOTANIMATION := device/oneplus/bacon/bootanimation.zip
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
+
+## Use the latest approved GMS identifiers unless running a signed build
+ifeq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/target/product/security/testkey)
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=oneplus/bacon/A0001:4.4.2/KVT49L/XNPH22R:user/release-keys PRIVATE_BUILD_DESC="bacon-user 4.4.2 KVT49L XNPH22R release-keys"
+endif
