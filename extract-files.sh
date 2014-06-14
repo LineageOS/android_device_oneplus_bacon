8777c3c854860951b5d1759d63f6c1c6c532c863
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-06-14 19:14:29 +0200

    bacon: Update extract file script
    * enable fetch file from a local dir use "-d <path>"
    * fix directory creation
    
    Change-Id: I200950088ccf523d30d4da4097a7152abc332512

--- extract-files.sh
+++ extract-files.sh
@@ -7,6 +7,18 @@ FP=$(cd ${0%/*} && pwd -P)
 export VENDOR=$(basename $(dirname $FP))
 export DEVICE=$(basename $FP)
 
+while getopts ":hd:" options
+do
+    case $options in
+        d ) LDIR=$OPTARG ;;
+        h ) echo "Usage: `basename $0` [OPTIONS] "
+            echo "  -d  Fetch blobs from local directory"
+            echo "  -h  Show this help"
+            exit ;;
+        * ) ;;
+    esac
+done
+
 function extract() {
     for FILE in `egrep -v '(^#|^$)' $1`; do
         OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
@@ -15,15 +27,24 @@ function extract() {
         if [ -z $DEST ]; then
             DEST=$FILE
         fi
-        DIR=`dirname $FILE`
+        DIR=`dirname $DEST`
         if [ ! -d $2/$DIR ]; then
             mkdir -p $2/$DIR
         fi
-        # Try CM target first
-        adb pull /system/$DEST $2/$DEST
-        # if file does not exist try OEM target
-        if [ "$?" != "0" ]; then
-            adb pull /system/$FILE $2/$DEST
+        if [ -z $LDIR ]; then
+            # Try CM target first
+            adb pull /system/$DEST $2/$DEST
+            # if file does not exist try OEM target
+            if [ "$?" != "0" ]; then
+                adb pull /system/$FILE $2/$DEST
+            fi
+        else
+            # Try CM target first
+            cp $LDIR/system/$DEST $2/$DEST
+            # if file does not exist try OEM target
+            if [ "$?" != "0" ]; then
+                cp $LDIR/system/$FILE $2/$DEST
+            fi
         fi
     done
 }
