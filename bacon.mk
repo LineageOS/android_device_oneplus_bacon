4966dc7e2c3459190f8eddae3866c159f0906bf9
Clement Yuan <spyworldxp@gmail.com>
2014-07-28 06:20:46 +0800

    add media codecs config files
    Change-Id: I5fa1aa1f3128aeb62111c7f93a7e5d96221b0f4f

--- bacon.mk
+++ bacon.mk
@@ -47,6 +47,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
 
+# Media Codecs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+
 # System properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=20
