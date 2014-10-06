09718b31dd67f49458950bab6b476d5656bedc47
Ethan Chen <intervigil@gmail.com>
2014-10-06 15:26:51 -0700

    bacon: Move mkbootimg to common repo
    Change-Id: Ia1e819de142a5bb86241f8113a99770fbc6fc2a3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,6 @@
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_bacon_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
-BOARD_CUSTOM_BOOTIMG_MK := device/oneplus/bacon/mkbootimg.mk
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oneplus/bacon/bluetooth
