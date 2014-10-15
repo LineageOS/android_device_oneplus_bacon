de250d152c70890193a810c239b8e28d642e8e37
Al Sutton <al@funkyandroid.com>
2014-10-15 13:00:22 +0100

    Add a modified list of extract files to oneplus/bacon device and create makefiles which don't include dependancies on the missing files.
    Some of the files listed in oppo/msm8974-common/proprietary-files-qc.txt do
    not exist in the 11S 38R firmware and so cause the extract to fail. This
    commit separates the two so the oppo/msm8974 common remains unchanged and
    the oneplus/bacon extract can find all the required files.
    
    For reference, the unavailable files were;
    
    vendor/lib/rfsa/adsp/libfastcvadsp.so
    vendor/lib/rfsa/adsp/libfastcvadsp_skel.so
    vendor/lib/rfsa/adsp/libapps_mem_heap.so
    vendor/lib/rfsa/adsp/libdspCV_skel.so
    vendor/lib/rfsa/adsp/libscveT2T_skel.so
    
    vendor/lib/libqct_resampler.so
    
    vendor/lib/liblistenhardware.so
    vendor/lib/liblistenjni.so
    -vendor/lib/liblisten.so
    vendor/lib/liblistensoundmodel.so
    vendor/lib/liblistensoundmodel2.so
    
    Change-Id: I0f6b17345aca255c3d3f068fb7f25942274cb848

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-export VENDOR=oneplus
-export DEVICE=bacon
-./../../oppo/msm8974-common/extract-files.sh $@
+function extract() {
+    for FILE in `egrep -v '(^#|^$)' $1`; do
+        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+        DEST=${PARSING_ARRAY[1]}
+        if [ -z $DEST ]; then
+            DEST=$FILE
+        fi
+        DIR=`dirname $FILE`
+        if [ ! -d $2/$DIR ]; then
+            mkdir -p $2/$DIR
+        fi
+        # Try CM target first
+        adb pull /system/$DEST $2/$DEST
+        # if file does not exist try OEM target
+        if [ "$?" != "0" ]; then
+            adb pull /system/$FILE $2/$DEST
+        fi
+    done
+}
+
+
+BASE=../../../vendor/oppo/msm8974-common/proprietary
+rm -rf $BASE/*
+
+DEVBASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $DEVBASE/*
+
+extract proprietary-files-qc.txt $BASE
+extract ../../oppo/msm8974-common/proprietary-files.txt $DEVBASE
+extract device-proprietary-files.txt $DEVBASE
+
+./../../oneplus/bacon/setup-makefiles.sh
