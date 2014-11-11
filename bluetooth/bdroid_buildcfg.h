232e6ca10ba32c8f4b4a9eaa7c1bdb3fb050e961
Steve Kondik <steve@cyngn.com>
2014-11-11 14:38:06 -0800

    bacon: Initial bringup for L
    Change-Id: I70c6642710d7a6b6dcb6cbe621a5a8325f3807e6

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BLUETOOTH_QCOM_SW TRUE
+#define BLUETOOTH_QTI_SW TRUE
 
 #define BTM_DEF_LOCAL_NAME   "OnePlus One"
 // Disables read remote device feature
