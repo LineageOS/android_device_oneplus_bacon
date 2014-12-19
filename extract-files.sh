1455b6a77173c4a9fff57d0115857266bc2e4aa9
Zyg0te <edvard.holst@gmail.com>
2014-12-19 08:42:52 +0100

    Bacon: A more elegant and OSX friendly extraction script
    A generic extract-files script that uses traditional sort as well as a OSX-friendly syntax.
    
    Change-Id: I1e7aed965e257916f5e41762759d0a6f35c0cac8

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-set -e
+#Use Traditional sorting
+export LC_ALL=C
 
-export VENDOR=oneplus
-export DEVICE=bacon
+FP=$(cd ${0%/*} && pwd -P)
+export VENDOR=$(basename $(dirname $FP))
+export DEVICE=$(basename $FP)
 ./../../oppo/msm8974-common/extract-files.sh $@
