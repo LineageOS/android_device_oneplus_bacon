c92e1019c9a606acce41157837a9b2c13c242b21
Steve Kondik <steve@cyngn.com>
2015-01-16 10:18:12 -0800

    bacon: Various updates
     * Bring in line with upstream
    
    Change-Id: Ia6e024e1bda34c11b981fbb8ebceb7f1ccaa255e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@
 
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_bacon_defconfig
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oneplus/bacon/bluetooth
