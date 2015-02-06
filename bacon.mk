0672cec2d25216258fa0d6cd04b8ac7fecefb21c
Danny Baumann <dannybaumann@web.de>
2015-02-06 15:00:51 +0100

    Decommonize audio_platform_info.xml (3/3)
    Change-Id: I169957faa46f233402b6af49e34bade044457912

--- bacon.mk
+++ bacon.mk
@@ -34,6 +34,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
