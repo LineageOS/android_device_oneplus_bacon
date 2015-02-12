a5d918c8f1bfcaff061ea27d588b88ab05afe8cb
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-02-12 08:42:51 +0100

    bacon: remove duplicated screen size definition
    already defined in bacon.mk
    
    Change-Id: I44ed3dfbdc5fa6b2c00f43994bb9233aa9547c9a

--- cm.mk
+++ cm.mk
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
