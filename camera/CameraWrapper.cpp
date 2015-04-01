2943f4aaa060d61a22140c9188a080c8262841e7
Steve Kondik <steve@cyngn.com>
2015-04-01 03:06:30 -0700

    bacon: More hacks to deal with vendor damage
    Change-Id: Ie64510e012de73474a0435e3e32eb6d8cb7ef8c8

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -372,10 +372,17 @@ static char *camera_get_parameters(struct camera_device *device)
         /* Disable 352x288 preview sizes, the combination of this preview size and larger resolutions stalls the HAL */
         params.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
             "1920x1080,1440x1080,1280x960,1280x720,768x432,720x480,640x480,576x432,384x288,320x240");
+        params.set(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES,
+            "4096x2160,3840x2160,1920x1080,1440x1080,1280x720,864x480,800x480,720x480,640x480,480x320,384x288,352x288,320x240,176x144");
+        params.set(CameraParameters::KEY_SUPPORTED_PICTURE_SIZES,
+            "4160x3120,4160x2340,4000x3000,4096x2160,3200x2400,3200x1800,2592x1944,2048x1536,1920x1080,1600x1200,1280x768,1280x720,1024x768,800x600,800x480,720x480,640x480,352x288,320x240");
+
+        params.set("supported-live-snapshot-sizes",
+            "3200x2400,2592x1944,2048x1536,1920x1080,1600x1200,1280x768,1280x720,1024x768,800x600,864x480,800x480,720x480,640x480,352x288,320x240");
     } else if (CAMERA_ID(device) == FRONT_CAMERA_ID) { 
         /* Inject all supported resolutions */
         params.set(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES,
-            "1280x720,864x480,800x480,720x480,640x480,480x320,384x288,352x288,320x240");
+            "1280x720,864x480,800x480,720x480,640x480,480x320,384x288,352x288,320x240,176x144");
     }
 
     return strdup(params.flatten().string());
