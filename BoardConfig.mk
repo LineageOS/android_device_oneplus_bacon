94d7220b2b3607a1add032f88d787a9995493376
Steve Kondik <steve@cyngn.com>
2015-10-19 14:43:11 -0700

    bacon: Support for native tap-to-wake
    Change-Id: I25f300f91c512a784b2a6c8c2b43544b3ed4e4eb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,7 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 # CM Hardware
 BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Enable transparent compression in the build
 # TARGET_TRANSPARENT_COMPRESSION_METHOD := lz4
