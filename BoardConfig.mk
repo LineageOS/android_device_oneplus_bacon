cf85feebca5779d45f9efd6ee36c20c59e8dad56
Steve Kondik <steve@cyngn.com>
2015-11-02 19:31:08 -0800

    bacon: Get rid of custom mkbootimg
     * We have generic support for this finally.
    
    Change-Id: I82facea2f29fc2f921bd5179f32f5b8ae82f9648

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,12 +42,12 @@ TARGET_CPU_VARIANT := krait
 TARGET_BOARD_INFO_FILE ?= $(PLATFORM_PATH)/board-info.txt
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(PLATFORM_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := cyanogenmod_bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
