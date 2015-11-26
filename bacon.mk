ee8f8ab972bfc6487293d8fb96ff987ec185663f
zh0122 <zh0122@gmail.com>
2015-11-26 11:24:38 +0800

    bacon: Remove duplicated property
    Change-Id: I5fceadbcc803e29a95c19bc5e06b26a245c469e4

--- bacon.mk
+++ bacon.mk
@@ -298,7 +298,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true \
     persist.timed.enable=true \
     ro.opengles.version=196608 \
-    ro.telephony.default_network=9 \
     ro.use_data_netmgrd=true \
     persist.data.netmgrd.qos.enable=true \
     persist.data.tcpackprio.enable=true \
