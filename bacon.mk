127a278156f519fdd1bf7a37c3956523b92d99bf
Steve Kondik <steve@cyngn.com>
2015-11-26 02:33:43 -0800

    bacon: Kill mm-parser
     * Qualcomm caused hell-on-earth with libavenhancements- an
       apparently well-intentioned effort to minimize the amount of
       change in f/av which has backfired. In order to use this
       library, an OEM must not change *anything* *anywhere* in f/av
       to maintain ABI exactness. And that ABI exactness is different
       per SoC branch and constantly in motion. Best of all, the
       base classes are monkey patched with a proprietary library which also
       is different per SoC branch and much exactly match the f/av
       code. And if you get the ABI right, then your customizations
       stop working because they've been hijacked by overloading. The whole
       message bus is hijacked, and you're wasting your time.
    
     * Now that FFMPEG can talk to all the hardware codecs, there is no
       reason to tolerate this. Our stack has more features, more codec
       support, and it's open source all the way.
    
     * I love Qualcomm's SoCs, but come on guys.. I expect every OEM
       will complain about this AV shit then proceed to gut it as I am doing.
    
    Change-Id: I8ed0de62d64cc10054933cd70a9958ea10359c7a

--- bacon.mk
+++ bacon.mk
@@ -74,7 +74,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     mm.enable.smoothstreaming=true \
-    mm.enable.qcom_parser=3310129 \
     ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.fluence.voicecall=true \
     audio.offload.buffer.size.kb=32 \
