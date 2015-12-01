78993524ccae4024abcba2d79767e42d337bfd20
Pat Erley <perley@cyngn.com>
2015-11-30 17:20:01 -0800

    bacon: enable subsystem restart on user builds
    CYNGNOS-1375
    Change-Id: I23b77f3c6c88601ceae3d9246a9af2cc090b1602
    (cherry picked from commit 9c89362539728083f1c3de9f34947dbe90eddf93)

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -29,6 +29,14 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.ssr.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.ssr.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom-common.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
