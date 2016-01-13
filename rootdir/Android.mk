ad872028c7b3890d5c18d90bab7ef6e2e9928eb0
Ethan Chen <intervigil@gmail.com>
2016-01-13 13:26:21 -0800

    bacon: Remove deprecated CNE init script
    Change-Id: Ibe29e032a31da9ca0d9fd2d5f3be556e94edf56d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -29,13 +29,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.cne.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.cne.rc
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.power.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
