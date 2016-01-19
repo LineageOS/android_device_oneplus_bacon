09e757d7ec863844c18ffaca5e675ddc19c3b6b7
Ethan Chen <intervigil@gmail.com>
2016-01-18 16:04:52 -0800

    bacon: Add xtwifi and lowi configuration
    * loc_launcher fails to even launch additional services without these
      configs
    
    Change-Id: I4a48caec5531edd276cff63b467327968d6efc90

--- bacon.mk
+++ bacon.mk
@@ -95,8 +95,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/lowi.conf:system/etc/lowi.conf \
     $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/xtwifi.conf:system/etc/xtwifi.conf
 
 # IPC router config
 PRODUCT_COPY_FILES += \
