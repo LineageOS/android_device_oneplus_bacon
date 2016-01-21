d2894bb8a85128fa7eb262a97521ee2e8bab66a2
Scott Mertz <scott@cyngn.com>
2016-01-21 10:37:05 -0800

    bacon: allow environment to set TARGET_KERNEL_ADDITIONAL_CONFIG
    cyanogenmod_kerneldebug_config contains additional debugging info.
    Allow this to be set by the environment.
    
    Change-Id: Ice1c178a7bc4fa5e0eb9772d3381e0d379b0d6b4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,7 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Enable DIAG on debug builds
 ifneq ($(TARGET_BUILD_VARIANT),user)
-TARGET_KERNEL_ADDITIONAL_CONFIG:= cyanogenmod_debug_config
+TARGET_KERNEL_ADDITIONAL_CONFIG ?= cyanogenmod_debug_config
 endif
 
 # ANT+
