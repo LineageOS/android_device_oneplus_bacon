5929f70fce5c7364d7e8c0e86d9fe2e7940c79cb
Khalid Zubair <kzubair@cyngn.com>
2016-01-27 15:58:43 -0800

    bacon: locsvc: fix log levels
    locsvc has runtime configurable logging levels but it prints
    everything out as ALOGE. Switch these macros out to use the
    corresponding Android log levels with the exception of LOGV.
    
    These headers are included elsewhere, so don't set LOG_NDEBUG as it
    might have side affects. Map LOGV to ALOGD instead.
    
    Change-Id: I611acc490da91adb512c9f50d80c53a64e0124db

--- gps/utils/log_util.h
+++ gps/utils/log_util.h
@@ -113,19 +113,19 @@ IF_LOC_LOGE { ALOGE("E/" __VA_ARGS__); } \
 else if (loc_logger.DEBUG_LEVEL == 0xff) { ALOGE("E/" __VA_ARGS__); }
 
 #define LOC_LOGW(...) \
-IF_LOC_LOGW { ALOGE("W/" __VA_ARGS__); }  \
+IF_LOC_LOGW { ALOGW("W/" __VA_ARGS__); }  \
 else if (loc_logger.DEBUG_LEVEL == 0xff) { ALOGW("W/" __VA_ARGS__); }
 
 #define LOC_LOGI(...) \
-IF_LOC_LOGI { ALOGE("I/" __VA_ARGS__); }   \
+IF_LOC_LOGI { ALOGI("I/" __VA_ARGS__); }   \
 else if (loc_logger.DEBUG_LEVEL == 0xff) { ALOGI("I/" __VA_ARGS__); }
 
 #define LOC_LOGD(...) \
-IF_LOC_LOGD { ALOGE("D/" __VA_ARGS__); }   \
+IF_LOC_LOGD { ALOGD("D/" __VA_ARGS__); }   \
 else if (loc_logger.DEBUG_LEVEL == 0xff) { ALOGD("D/" __VA_ARGS__); }
 
 #define LOC_LOGV(...) \
-IF_LOC_LOGV { ALOGE("V/" __VA_ARGS__); }   \
+IF_LOC_LOGV { ALOGD("V/" __VA_ARGS__); }   \
 else if (loc_logger.DEBUG_LEVEL == 0xff) { ALOGV("V/" __VA_ARGS__); }
 
 #else /* DEBUG_DMN_LOC_API */
