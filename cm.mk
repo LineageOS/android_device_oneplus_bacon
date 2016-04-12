2249c799e2082b10a2a5358b2183851c63d3c6df
Pat Erley <perley@cyngn.com>
2016-04-12 11:14:43 -0700

    bacon: Update Build Fingerprint
    oneplus/bacon/A0001:6.0.1/MMB29X/ZNH0EAS2JK:user/release-keys
    
    Change-Id: I721d6f461a84e1f7c45a9169926404e6a1ec20d8

--- cm.mk
+++ cm.mk
@@ -40,6 +40,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
 ## Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=oneplus/bacon/A0001:5.1.1/LMY48B/YOG4PAS1N0:user/release-keys \
-    PRIVATE_BUILD_DESC="bacon-user 5.1.1 LMY48B YOG4PAS1N0 release-keys"
+    BUILD_FINGERPRINT=oneplus/bacon/A0001:6.0.1/MMB29X/ZNH0EAS2JK:user/release-keys \
+    PRIVATE_BUILD_DESC="bacon-user 6.0.1 MMB29X ZNH0EAS2JK release-keys"
 endif
