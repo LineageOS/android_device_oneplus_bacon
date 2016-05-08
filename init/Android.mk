3ca2cd741e5aeee43b924faf86452fa11975cda6
Michael Bestas <mikeioannina@gmail.com>
2016-05-08 23:44:16 +0300

    bacon: Switch to standalone libinit
    * No need to build the msm one
    
    Change-Id: I181e2feccea1aa830010b9b826848d2202e83c43

---
+++ init/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := system/core/init
+LOCAL_CFLAGS := -Wall
+LOCAL_SRC_FILES := init_bacon.cpp
+LOCAL_MODULE := libinit_bacon
+
+include $(BUILD_STATIC_LIBRARY)
