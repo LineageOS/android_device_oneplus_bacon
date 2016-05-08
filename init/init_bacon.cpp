efe42531c8ad223eb1d530aef0d83cd2b9ae5009
Michael Bestas <mikeioannina@gmail.com>
2016-05-08 23:44:16 +0300

    bacon: Switch to standalone libinit
    * No need to build the msm one
    
    Change-Id: I181e2feccea1aa830010b9b826848d2202e83c43

--- init/init_bacon.cpp
+++ init/init_bacon.cpp
@@ -27,12 +27,67 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
+
+static int read_file2(const char *fname, char *data, int max_size)
+{
+    int fd, rc;
+
+    if (max_size < 1)
+        return 0;
+
+    fd = open(fname, O_RDONLY);
+    if (fd < 0) {
+        ERROR("failed to open '%s'\n", fname);
+        return 0;
+    }
+
+    rc = read(fd, data, max_size - 1);
+    if ((rc > 0) && (rc < max_size))
+        data[rc] = '\0';
+    else
+        data[0] = '\0';
+    close(fd);
+
+    return 1;
+}
+
+static void init_alarm_boot_properties()
+{
+    char const *alarm_file = "/proc/sys/kernel/boot_reason";
+    char buf[64];
+
+    if (read_file2(alarm_file, buf, sizeof(buf))) {
+        /*
+         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+         * For existing PMIC chips, the following mapping applies
+         * for the value of boot_reason:
+         *
+         * 0 -> unknown
+         * 1 -> hard reset
+         * 2 -> sudden momentary power loss (SMPL)
+         * 3 -> real time clock (RTC)
+         * 4 -> DC charger inserted
+         * 5 -> USB charger insertd
+         * 6 -> PON1 pin toggled (for secondary PMICs)
+         * 7 -> CBLPWR_N pin toggled (for external power supply)
+         * 8 -> KPDPWR_N pin toggled (power key pressed)
+         */
+        if (buf[0] == '3')
+            property_set("ro.alarm_boot", "true");
+        else
+            property_set("ro.alarm_boot", "false");
+    }
+}
 
 static void import_kernel_nv(char *name, int for_emulator)
 {
@@ -45,14 +100,13 @@ static void import_kernel_nv(char *name, int for_emulator)
 
     if (!strcmp(name,"oppo.rf_version")) {
         property_set("ro.oppo.rf_version", value);
-    }
-    else if (!strcmp(name,"oppo.pcb_version")) {
+    } else if (!strcmp(name,"oppo.pcb_version")) {
         property_set("ro.oppo.pcb_version", value);
     }
 }
 
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
+void vendor_load_properties()
 {
     import_kernel_cmdline(0, import_kernel_nv);
+    init_alarm_boot_properties();
 }
-
