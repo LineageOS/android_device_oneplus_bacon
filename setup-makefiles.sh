d46a5a156b823d5dd22b7491ba62c76a7cfa352e
Steve Kondik <steve@cyngn.com>
2016-05-18 12:54:14 -0700

    bacon: Reenable the QC location stack
     * Mystery solved, it works.
    
    This reverts commit 7cef4f344e5d09a9839a9e3d027a19089e6cd309.
    
    Change-Id: I6c33e07c517702331f05650dc85c84ded9781ec1

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -99,6 +99,7 @@ DEVICE_PACKAGE_OVERLAYS += vendor/$VENDOR/$DEVICE/overlay
 
 PRODUCT_PACKAGES += \\
     CNEService \\
+    com.qualcomm.location \\
     dpmserviceapp \\
     qcrilmsgtunnel \\
     shutdownlistener
@@ -173,6 +174,17 @@ include \$(CLEAR_VARS)
 LOCAL_MODULE := CNEService
 LOCAL_MODULE_OWNER := $VENDOR
 LOCAL_SRC_FILES := proprietary/priv-app/CNEService/CNEService.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
