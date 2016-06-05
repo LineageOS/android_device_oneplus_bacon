87170a2ad70c347a5766fb764664f472ed347b6e
Steve Kondik <steve@cyngn.com>
2016-06-05 00:07:58 -0700

    bacon: Disable memory compression
     * Turned this on as an experiment, and am now turning both off.
       I don't see any measurable performance gains, but I do
       unfortunately see higher overall power numbers.
     * ZRAM also has a tendency to make the device sluggish after
       several days of usage. Perhaps this has been fixed in newer
       kernel releases (it's a problem on 3.10 too), but our
       trusty old Baconator does just fine with good old fashioned
       uncompressed memory. Let's leave ZRAM for devices with
       less than 2GB or less memory.
    
    Change-Id: Iea77aae56fbc86bd48702e2e0d76fe82d6579fc0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,7 @@ TARGET_BOARD_INFO_FILE ?= $(PLATFORM_PATH)/board-info.txt
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 zcache.enabled=1 zcache.compressor=lz4
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
