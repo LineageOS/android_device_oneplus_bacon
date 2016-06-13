8f2fc511583da7ca44e9e104790136d5a179ab9f
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-06-13 21:27:43 +0300

    cmhw: Use cmsdk FileUtils
    Change-Id: Idebbc357888c8670829a70c69c2fa99f717b45c6

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -16,7 +16,7 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 import java.io.File;
 
 public class VibratorHW {
