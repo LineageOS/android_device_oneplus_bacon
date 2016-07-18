7599a4a19612935cdbb870bb66085d2b0afd21aa
Steve Kondik <steve@cyngn.com>
2016-07-18 12:27:14 -0700

    bacon: Use the new generic extractor
    Change-Id: I80c5887405e587f1f854f5991d1a9b604a4c84e2

--- extract-files.sh
+++ extract-files.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-#set -e
-export DEVICE=bacon
-export VENDOR=oneplus
+set -e
+
+DEVICE=bacon
+VENDOR=oneplus
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
 
 if [ $# -eq 0 ]; then
   SRC=adb
@@ -20,42 +49,10 @@ else
   fi
 fi
 
-function extract() {
-    for FILE in `egrep -v '(^#|^$)' $1`; do
-      OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-      FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-      DEST=${PARSING_ARRAY[1]}
-      if [ -z $DEST ]
-      then
-        DEST=$FILE
-      fi
-      DIR=`dirname $DEST`
-      if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-      fi
-      # Try CM target first
-      if [ "$SRC" = "adb" ]; then
-        adb pull /system/$DEST $BASE/$DEST
-        # if file does not exist try OEM target
-        if [ "$?" != "0" ]; then
-            adb pull /system/$FILE $BASE/$DEST
-        fi
-      else
-        if [ -z $SRC/system/$DEST ]; then
-            echo ":: $DEST"
-            cp $SRC/system/$DEST $BASE/$DEST
-        else
-            echo ":: $FILE"
-            cp $SRC/system/$FILE $BASE/$DEST
-        fi
-      fi
-    done
-}
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+extract "$MY_DIR"/proprietary-files-qc.txt "$SRC"
+extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
-extract ../../$VENDOR/$DEVICE/proprietary-files-qc.txt $BASE
-extract ../../$VENDOR/$DEVICE/proprietary-files.txt $BASE
-
-./setup-makefiles.sh
+"$MY_DIR"/setup-makefiles.sh
