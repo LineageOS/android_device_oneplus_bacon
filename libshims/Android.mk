e29467ddabc09e0fc3f97bd9ec2d4e0088355b47
Arne Coucheron <arco68@gmail.com>
2016-11-05 19:13:10 +0200

    bacon: Add shim for libwvm.so
    Change-Id: I1ac58a108783ae1aabe3f3f061b2444925f684db

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,15 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    camera_shim.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libui
-
+LOCAL_SRC_FILES := camera_shim.cpp
+LOCAL_SHARED_LIBRARIES := libui
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
-LOCAL_MULTILIB := 32
+include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := wvm_shim.cpp
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_MODULE := libshim_wvm
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
