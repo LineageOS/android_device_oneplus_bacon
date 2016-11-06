b7b6d94529e17ce51566aa6509cebab6436b153d
Simon Shields <keepcalm444@gmail.com>
2016-11-06 13:33:44 +1100

    bacon: disable CNE/DPM/QC location
    Revert "bacon: Enable CNE + DPM"
    
    This reverts commit 06b73aceac504c9e61282b201b29e6b7f6781dea.
    
    Revert "bacon: Reenable the QC location stack"
    
    This reverts commit 018298e831be2a2a8a7a7daecc6cdb3b0a161f29.
    
    Change-Id: If46a8ff8cd0862280aa9991bb0ec1d9a88c583ce

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # GPS
+BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 USE_DEVICE_SPECIFIC_GPS := true
 USE_DEVICE_SPECIFIC_LOC_API := true
 
@@ -183,16 +184,14 @@ WPA_SUPPLICANT_VERSION           := VER_0_8_X
 # Inherit from QC proprietary
 ifneq ($(QCPATH),)
 -include $(QCPATH)/common/msm8974/BoardConfigVendor.mk
-endif
 
 # Bluetooth
 FEATURE_QCRIL_UIM_SAP_SERVER_MODE := true
 
 # QCNE
-BOARD_USES_QCNE := true
-
 ifeq ($(BOARD_USES_QCNE),true)
 TARGET_LDPRELOAD := libNimsWrap.so
+endif
 endif
 
 -include vendor/oneplus/bacon/BoardConfigVendor.mk
