f81aefe0dde04a6581faa02c0ff433878eef01f6
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 02:36:33 -0700

    bacon: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8

--- cm.mk
+++ cm.mk
@@ -18,9 +18,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bacon device
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
