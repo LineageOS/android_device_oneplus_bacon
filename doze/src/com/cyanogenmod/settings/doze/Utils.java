9ee13e031633cfd1c0d74c4db94db4dabcfdfd90
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-12-04 11:29:07 +0530

    bacon: doze: Commonize strings translations
     * Translations moved to the new project
        android_packages_resources_devicesettings
    
    Change-Id: I06536915e1963a2fe464c3d6c46dd8728aba0cb4
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- doze/src/com/cyanogenmod/settings/doze/Utils.java
+++ doze/src/com/cyanogenmod/settings/doze/Utils.java
@@ -34,7 +34,7 @@ public final class Utils {
     private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
 
     protected static final String AMBIENT_DISPLAY_KEY = "doze_enabled";
-    protected static final String PICK_UP_KEY = "pick_up";
+    protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
 
@@ -74,7 +74,7 @@ public final class Utils {
 
     protected static boolean pickUpEnabled(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
-                .getBoolean(PICK_UP_KEY, false);
+                .getBoolean(GESTURE_PICK_UP_KEY, false);
     }
 
     protected static boolean handwaveGestureEnabled(Context context) {
