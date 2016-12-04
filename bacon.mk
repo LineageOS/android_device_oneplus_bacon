d963f299c2557a3ea29b36cea12f1008c6f7b205
Simon Shields <keepcalm444@gmail.com>
2016-12-05 00:35:31 +1100

    bacon: ship OneplusDoze
    Change-Id: I44aef2adb6bb0f9938a311a13abda24a61e8d12d

--- bacon.mk
+++ bacon.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Doze
+PRODUCT_PACKAGES += \
+	OneplusDoze
+
 # Gello
 PRODUCT_PACKAGES += \
     Gello
