3ae5a9520c3129992d1a471212887cab6959e682
Michael Bestas <mkbestas@lineageos.org>
2017-06-17 23:21:20 +0300

    bacon: Use widevine from kipper TOS125B
    Change-Id: Ib6c975cbb227edc537b6429600ae3e296c7a74dc

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -6,10 +6,3 @@ LOCAL_SHARED_LIBRARIES := libui
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := wvm_shim.cpp
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-LOCAL_MODULE := libshim_wvm
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
