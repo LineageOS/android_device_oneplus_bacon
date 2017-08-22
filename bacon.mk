38c5574b594438050e3da38a2f6a86697a28acb2
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:01:07 -0700

    bacon: Move Lineage specific overlays to overlay-lineage
    rebrand CMParts overlays while we're at it
    
    Change-Id: Ic38bb1acb4d37f90b62679e4420815f49b30fc7f

--- bacon.mk
+++ bacon.mk
@@ -19,7 +19,9 @@ $(call inherit-product-if-exists, $(QCPATH)/common/config/device-vendor.mk)
 endif
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+	$(LOCAL_PATH)/overlay \
+	$(LOCAL_PATH)/overlay-lineage
 
 # ANT+
 PRODUCT_PACKAGES += \
