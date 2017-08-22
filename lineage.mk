34d2818e61c4fee71ad62493f7decabc7cbf1ee2
Simon Shields <simon@lineageos.org>
2017-08-23 00:32:04 +1000

    bacon: s@vendor/cm@vendor/lineage@g
    Change-Id: Ib4553f64ca461fd395675b53b526716a528cac69

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_bacon
 PRODUCT_DEVICE := bacon
