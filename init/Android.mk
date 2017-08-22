72ac0474710632d4e3547f6b380a7ce6c28afd34
Simon Shields <simon@lineageos.org>
2017-08-23 01:01:40 +1000

    bacon: init: use libbase for log headers
    Change-Id: I05618bd1de934cc41790d77189bd5971e417c96d

--- init/Android.mk
+++ init/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall
+LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_SRC_FILES := init_bacon.cpp
 LOCAL_MODULE := libinit_bacon
 
