5941a015aa91292986166badcb7c839f0a4cf64f
Simon Shields <simon@lineageos.org>
2017-08-23 00:51:12 +1000

    DO NOT MERGE: bacon: nuke ANT+
    Change-Id: Iccb9b58ea421ceb5a300a003cda50dd5b658906d

--- bacon.mk
+++ bacon.mk
@@ -22,10 +22,10 @@ endif
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay vendor/extra/overlays/phone-1080p
 
 # ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
+#PRODUCT_PACKAGES += \
+#    AntHalService \
+#    com.dsi.ant.antradio_library \
+#    libantradio
 
 # API (for CTS backward compatibility)
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -190,7 +190,6 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
