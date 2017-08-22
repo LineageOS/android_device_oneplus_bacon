241f2ff1e19333178a192bec9c83fbf639a78867
Simon Shields <simon@lineageos.org>
2017-08-23 00:32:04 +1000

    bacon: s@vendor/cm@vendor/lineage@g
    Change-Id: I472b1c9e31e81671bcb9e09f43d5c497395d662a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -29,7 +29,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 CM_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
