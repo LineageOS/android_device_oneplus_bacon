32b5d1aecfa62dd635ef293470057d7eba76f6de
Simon Shields <simon@lineageos.org>
2017-08-27 02:46:43 +1000

    HACK: bacon: bluetooth: disable BLE_VND_INCLUDED
    causes bluetooth to hang and crash repeatedly
    
    Change-Id: I5f12e9ab7916a5df73b29275c85ed4fa144f78ef

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -23,7 +23,7 @@
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
+//#define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
 #define BLE_PERIPHERAL_ADV_NAME  FALSE
