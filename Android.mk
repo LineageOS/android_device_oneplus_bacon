a15621547f0a766ec0048bb6d9d226be020fafdf
Bruno Martins <bgcngm@gmail.com>
2017-09-04 00:40:11 +0100

    bacon: Move WiFi files to vendor
     * The config is now expected to be placed there.
    
     * Move the rest of it to be coherent.
    
    Change-Id: Ib6fdbb23df25c8ebfebb7b5c58005723b3348892

--- Android.mk
+++ Android.mk
@@ -55,7 +55,7 @@ WCNSS_IMAGES := \
     wcnss.b00 wcnss.b01 wcnss.b02 wcnss.b04 wcnss.b06 \
     wcnss.b07 wcnss.b08 wcnss.b09 wcnss.mdt
 
-WCNSS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(WCNSS_IMAGES)))
+WCNSS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(WCNSS_IMAGES)))
 $(WCNSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "WCNSS firmware link: $@"
 	@mkdir -p $(dir $@)
