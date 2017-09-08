9cc8b440b854320ffca1919879ce6a0deb04db8d
Bruno Martins <bgcngm@gmail.com>
2017-09-09 00:52:49 +0100

    init: Use core init function to read from files
    Change-Id: I3fd23490bcdfa4097dad73161ad226337e93cd18

--- init/Android.mk
+++ init/Android.mk
@@ -3,7 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
 LOCAL_CFLAGS := -Wall
 LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_SRC_FILES := init_bacon.cpp
--- init/init_bacon.cpp
+++ init/init_bacon.cpp
@@ -34,40 +34,21 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include <android-base/strings.h>
+
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
 
-static int read_file2(const char *fname, char *data, int max_size)
-{
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        LOG(ERROR) << "failed to open '" << fname << "'\n";
-        return 0;
-    }
-
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
+using android::base::Trim;
 
 static void init_alarm_boot_properties()
 {
-    char const *alarm_file = "/proc/sys/kernel/boot_reason";
-    char buf[64];
+    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
+    std::string boot_reason;
 
-    if (read_file2(alarm_file, buf, sizeof(buf))) {
+    if (read_file(boot_reason_file, &boot_reason) {
         /*
          * Setup ro.alarm_boot value to true when it is RTC triggered boot up
          * For existing PMIC chips, the following mapping applies
@@ -83,7 +64,7 @@ static void init_alarm_boot_properties()
          * 7 -> CBLPWR_N pin toggled (for external power supply)
          * 8 -> KPDPWR_N pin toggled (power key pressed)
          */
-        if (buf[0] == '3')
+        if (Trim(boot_reason) == "3")
             property_set("ro.alarm_boot", "true");
         else
             property_set("ro.alarm_boot", "false");
