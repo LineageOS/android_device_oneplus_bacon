b2b941f3d60e310b6d91234633f849817a0fc13b
Michael Bestas <mkbestas@lineageos.org>
2017-09-28 02:44:54 +0300

    bacon: Cleanup bluetooth config
    * Remove unused flags
    * MAX_L2CAP_CHANNELS defaults to 16
    
    Change-Id: Ide4ecba5eda5fb96d2cf3bf74217334216287cb1

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,16 +17,9 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BLUETOOTH_QTI_SW TRUE
-
 #define BTM_DEF_LOCAL_NAME   "OnePlus One"
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
-#define BT_CLEAN_TURN_ON_DISABLED 1
+#define BLE_VND_INCLUDED          TRUE
+#define BLUETOOTH_QTI_SW          TRUE
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
 
 #endif
