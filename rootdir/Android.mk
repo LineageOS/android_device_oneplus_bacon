31985a7e1274b910e4d5e083971ed41a0edd7cd2
Ethan Chen <intervigil@gmail.com>
2017-09-29 23:14:23 -0700

    bacon: Remove init.qcom.bt.sh
    * Just run the hci_qcomm_init binary instead
    
    Change-Id: If2b4b9fc738e729bd389f3f96e6843a4ca6eb558

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,15 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-# QCOM config scripts
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.bt.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
-include $(BUILD_PREBUILT)
-
 # Device init files
 
 include $(CLEAR_VARS)
