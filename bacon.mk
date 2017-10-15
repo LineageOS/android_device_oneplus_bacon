7c2f9bc6baf632151cab3dcd40f0a50f7b6c64b2
Michael Bestas <mkbestas@lineageos.org>
2017-10-15 18:01:00 +0300

    bacon: Remove unused quipc.conf
    Change-Id: I66391197c5bbf4e754d80dee492ab4b4096e811c

--- bacon.mk
+++ bacon.mk
@@ -123,7 +123,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # HIDL
