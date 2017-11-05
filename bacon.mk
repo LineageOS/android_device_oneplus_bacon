a216b6afaa59fe505083824d17c798efaa4b2b8a
jrior001 <jriordan001@gmail.com>
2017-11-04 23:42:59 -0400

    bacon: update DRM support
    Change-Id: I5a9771a6965937cf1371041c82785bde37529fee

--- bacon.mk
+++ bacon.mk
@@ -98,6 +98,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
