97791d1d8d74240ab41b56734abfe3c77d015a0d
Paul Keith <javelinanddart@gmail.com>
2017-12-02 21:59:59 +0100

    bacon: Remove TARGET_KERNEL_CROSS_COMPILE_PREFIX
    * We want to use the default androidkernel- toolchain
      that doesn't use the gold linker that breaks unwinding
    
    Change-Id: Iadd28af2e2a7b6d404bb625ecaaf0bd9bbcff899

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,6 @@ BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := lineageos_bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
