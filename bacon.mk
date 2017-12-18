a14fd06b8b6c648ee5d6109530bb29ebc19f6612
Bruno Martins <bgcngm@gmail.com>
2017-12-18 10:05:23 +0000

    bacon: Convert audio_effects.conf to XML
     * Based on https://android.googlesource.com/device/google/taimen/+/ecf82951a431324bd129b31495043dcfa00cfb1f
    
     * Additionally to the same tests done by Google, it was verified that
       with this change "XML effect configuration loaded successfully.",
       while before it showed: "XML effect configuration failed to load.".
    
    Change-Id: I42112845def8a4a88022a0319c46ffb178d30ff2

--- bacon.mk
+++ bacon.mk
@@ -44,7 +44,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/MTP_Speaker_cal.acdb:system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
