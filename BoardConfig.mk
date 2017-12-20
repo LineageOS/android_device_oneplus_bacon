368f92d682df898aad2ab328fcf2ff50b995251e
Michael Bestas <mkbestas@lineageos.org>
2017-12-21 01:35:03 +0200

    bacon: Include legacy qcom sepolicy rules
    Change-Id: Ic499de98c34a63de18e935d2cc6f23f0e69b1a43

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,6 +163,7 @@ TARGET_NO_RPC := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     $(PLATFORM_PATH)/sepolicy
