812b3031dd2577aa0162890aa5d3d6ee385961c1
jrior001 <jriordan001@gmail.com>
2018-01-11 21:11:57 -0500

    bacon: move to oppo_8974 kernel
    Change-Id: I5719a23a41f3f03c407ca5b00f764eace7f3e1d3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := lineageos_bacon_defconfig
-TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
+TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # ANT+
