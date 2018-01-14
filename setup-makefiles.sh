d8664b600334966040c6ab3e2814da8a4262ac0b
jrior001 <jriordan001@gmail.com>
2018-01-14 17:00:47 -0500

    bacon: use the common proprietary blobs
    Change-Id: I39cdd26cb3f120e553cf91c0c440893e72cffdc1

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,44 +17,8 @@
 
 set -e
 
-export INITIAL_COPYRIGHT_YEAR=2014
+export DEVICE=bacon
+export DEVICE_COMMON=msm8974-common
+export VENDOR=oneplus
 
-# Required!
-DEVICE=bacon
-VENDOR=oneplus
-
-# Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-CM_ROOT="$MY_DIR"/../../..
-
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-# Copyright headers and guards
-write_headers
-
-# The standard blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
-
-# Qualcomm BSP blobs - we put a conditional around here
-# in case the BSP is actually being built
-printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$PRODUCTMK"
-printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$ANDROIDMK"
-
-write_makefiles "$MY_DIR"/proprietary-files-qc.txt
-
-printf '%s\n' "endif" >> "$PRODUCTMK"
-printf '%s\n' "endif" >> "$ANDROIDMK"
-
-# We are done!
-write_footers
-
+./../../oppo/msm8974-common/setup-makefiles.sh $@
