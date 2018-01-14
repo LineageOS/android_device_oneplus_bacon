911d58768e60e923895bfde26529b4ff8a46d500
jrior001 <jriordan001@gmail.com>
2018-01-14 17:07:16 -0500

    DO NOT MERGE: bacon: boot permissive
    Change-Id: Ibdcafa5e5af1d164de2c0b01196f1463badf3318

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@
 -include device/oppo/msm8974-common/BoardConfigCommon.mk
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
 TARGET_KERNEL_CONFIG := lineageos_bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
 
