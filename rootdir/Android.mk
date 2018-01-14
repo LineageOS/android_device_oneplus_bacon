5254dc4ff3940d0c273d4c83ed0aec4b72b8b468
jrior001 <jriordan001@gmail.com>
2018-01-13 19:25:21 -0500

    bacon: inherit msm8974-common again
    Change-Id: I468df736282d7193e03023c5e582d7b1ffb5a87b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -20,14 +20,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.power.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
