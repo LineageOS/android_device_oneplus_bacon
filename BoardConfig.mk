af5ed9dc97da6988ccac814c5eb0b3d3ff5e1782
Simon Shields <simon@lineageos.org>
2018-01-15 22:19:08 +1100

    bacon: use oppo 8974-common and our own system.prop
    Change-Id: I21394812860fcc748cc8b34ab3c691c21691760c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,9 @@ TARGET_LIBINIT_MSM8974_DEFINES_FILE := device/oneplus/bacon/init/init_bacon.cpp
 # NFC
 BOARD_NFC_CHIPSET := pn547
 
+# Properties
+TARGET_SYSTEM_PROP += device/oneplus/bacon/system.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := device/oneplus/bacon/rootdir/etc/fstab.bacon
 
