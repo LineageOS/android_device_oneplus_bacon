3ab7372aea946af4009064817a4aa5f9ec78254b
Michael Bestas <mkbestas@lineageos.org>
2018-01-17 19:17:45 +0200

    bacon: Move permissions to vendor
    Change-Id: I2aaba55284436014b461bcb9adee0bb2db5327b2

--- bacon.mk
+++ bacon.mk
@@ -55,9 +55,9 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/vendor/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/vendor/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/vendor/etc/permissions/com.android.nfc_extras.xml \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
