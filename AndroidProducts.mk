4fd9c0562484d6428ed0cd9524948024022a7daa
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    bacon: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_bacon.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_bacon-eng \
+    lineage_bacon-userdebug
