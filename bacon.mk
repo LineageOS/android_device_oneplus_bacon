963cc9ed8b4d4679ea75e2b4b3d6141f8ec54d5d
Martin Otto <paraboloid234@gmail.com>
2018-06-27 12:01:49 +0200

    Revert "bacon: we have a device with 3GB RAM"
    Causes stability issues (like crashes), performance-heavy games are affected mostly.
    
    This reverts commit 4730417d9ab1a98fa46b3691961d0ff5b0c8ea0a.
    
    Change-Id: I73c4c6ad131f1ed7a6a138d1f1af3a9bf12fdc6f

--- bacon.mk
+++ bacon.mk
@@ -34,8 +34,7 @@ PRODUCT_PACKAGES += \
     camera.msm8974
 
 # Dalvik/HWUI
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
 # Display
 PRODUCT_AAPT_CONFIG := normal
