a36cd3d9f1bfa178116649a77a5fb873fd42190f
Michael Bestas <mkbestas@lineageos.org>
2019-08-06 19:31:49 +0000

    bacon: Inherit common_mini_phone
    * We don't have enough system space to fit anything larger
      that pico GApps packages, so use the mini target while
      still building Recorder. This excludes about 25MB of
      LatinIME dictionaries.
    
    Change-Id: I418f1729df20da16a184d02e7e15c64df1053374

--- lineage_bacon.mk
+++ lineage_bacon.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
 
 PRODUCT_NAME := lineage_bacon
 PRODUCT_DEVICE := bacon
