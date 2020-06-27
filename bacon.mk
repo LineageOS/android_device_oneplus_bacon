26c4d1a7b26575c85ce370f0f0f05111d9f4b61e
Nolen Johnson <johnsonnolen@gmail.com>
2020-06-26 21:08:17 -0400

    bacon: Update nfc_extras permission path
    Change-Id: I59b83d1537c4604057e623848f969b0afed2377d

--- bacon.mk
+++ bacon.mk
@@ -57,7 +57,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
